d5cb25b357609fb157480edb64e37103918e99e7
shangxian Piao <sj212131@hotmail.com>
2021-02-20 15:46:03 -0500

    upload basic file

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById("startbtn")
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Game start')
+}
+
+function nextQuestion() {
+
+}
+
+function selectAnswer(){
+
+}
