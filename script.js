63225a26ca4c1de4a6bc0b3154b286e4cc6fa20e
shangxian Piao <sj212131@hotmail.com>
2021-02-21 01:45:58 -0500

    updated user selection, correction, timer

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ var startButton = document.getElementById("startbtn");
 var questionCard = document.getElementById("question-container");
 var questionEL = document.getElementById("question")
 var answerButtenEl = document.getElementById("answerbtn")
-
+var timeEl = document.querySelector(".time");
+var inputInt = document.getElementById("intBox")
+var saveBtn = document.getElementById("saveBtn")
 
 var userscore = 0
+var secondsLeft = 30;
 
 //question bank
 var questions = [
@@ -43,26 +46,14 @@ var questions = [
             { text: "msgBox('Hello World!')", correct: false},
             { text: "prompt('Hello World!')", correct: false },
         ] 
+    },
+    {
+        question: 'Game is over!'
     }
 ]
 
 //click to start
 startButton.addEventListener('click', startGame);
-
-//test of question log
-console.log(questions[0].question)
-console.log(questions[1].question)
-console.log(questions[2].question)
-console.log(questions[3].question)
-//test of answer log
-console.log(questions[0].answers)
-console.log(questions[0].answers[0])
-console.log(questions[0].answers[0].text)
-console.log(questions[0].answers.length)
-console.log(questions[1].answers)
-console.log(questions[2].answers)
-console.log(questions[3].answers)
-console.log(questions[Math.floor(Math.random() * questions.length)].question)
 
 // random question
 var question1 = questions[0]
@@ -70,46 +61,115 @@ var question2 = questions[1]
 var question3 = questions[2]
 var question4 = questions[3]
 
-// var randomQuestion = questions[Math.floor(Math.random() * questions.length)].question
-
 //Game start
 function startGame() {
+    questionIndex = 0;
     console.log('Game start');
     startButton.classList.add('hide');
+    inputInt.classList.add('hide');
+    saveBtn.classList.add('hide');
     questionCard.classList.remove('hide');
-    var selectionBtn = document.querySelector('.answer-btns');
-    selectionBtn.addEventListener('click', nextQuestion)
     questionDisplay();
+    selection();
+    setTime();
 }
 
+//timer
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timeEl.textContent = secondsLeft + " Sec left!";
+      if(secondsLeft == 0 || questionIndex == 4) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        secondsLeft = 30;
+        // Calls function to create and append image
+        endMessage();
+      }
+    }, 1000);
+}
+
+
+
+function endMessage() {
+    timeEl.textContent = " Game over !";
+    questionEL.innerHTML = questions[4].question;
+    questionCard.classList.add('hide');
+
+    startButton.classList.remove('hide');
+    inputInt.classList.remove('hide');
+    saveBtn.classList.remove('hide');
+    startButton.innerText = "Restart"
+    secondsLeft = 30;
+}
 var questionIndex = 0;
-var currentlyQuestion = questions[questionIndex];
+var currentlyQuestion = questions[questionIndex];   
 
 //Game display on the card
-function questionDisplay(currentlyQuestion) {
+function questionDisplay() {
     console.log(questions[questionIndex].question)
     //question display
     answerA = document.getElementById("answerA")
     answerB = document.getElementById("answerB")
     answerC = document.getElementById("answerC")
     answerD = document.getElementById("answerD")
+
+    if (questionIndex == 4) {
+        endMessage();
+        return
+    }
     
-    // for (var i = 0; i < questions.length; i++) {
+    // Answers for different questions 
     questionEL.innerHTML = questions[questionIndex].question;
     answerA.innerText = questions[questionIndex].answers[0].text;
     answerB.innerText = questions[questionIndex].answers[1].text;
     answerC.innerText = questions[questionIndex].answers[2].text;
     answerD.innerText = questions[questionIndex].answers[3].text;
-
 }
 
-console.log(questionIndex);
+//next question
 function nextQuestion() {
-    if (questions[questionIndex].answers.correct) {
-        userscore++;
-    }
-    console.log(questions[questionIndex].answers.correct)
-    console.log(userscore)
     questionIndex++;
     questionDisplay(questions[questionIndex].question);
+    console.log(questionIndex)
+    if (questionIndex == 4) {
+        endMessage();
+        return
+    }
 }
+
+//corrections
+function selection() {
+    correction();
+    var selectionBtn = document.querySelector('.answer-btns');
+    console.log(selectionBtn)
+    selectionBtn.addEventListener('click', nextQuestion)
+}
+
+function correction(e) {
+    var selcetedBtn = e.target
+    var correct = selcetedBtn.dataset.correct
+    console.log(correct)
+}
+
+function correct() {
+    userscore++;
+    localStorage.setItem("Highest score: ", userscore)
+}
+
+function wrong() {
+    secondsLeft -= 5
+}
+
+saveBtn.addEventListener('click', saveDate);
+
+function saveDate() {
+    e.preventDefault();
+    localStorage.setItem("score", userscore);
+}
+
+
+
+
+
